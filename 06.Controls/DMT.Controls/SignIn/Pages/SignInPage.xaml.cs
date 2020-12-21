320bb8ed968a74144d6c4faa22c59473fe618762
chumpon-asaneerat <chumponsenate@gmail.com>
2020-12-21 14:22:25 +0700

    Ported DMTv2 to DMT.TA.TODv1, DMT.TA.TODv2 #341 - DMT.TAxTODv2   - DMT.Controls project.     - Ported SmartcardManager     - Ported UserSearchManager     - Ported SignInPage     - Ported SignInWindow

---
+++ 06.Controls/DMT.Controls/SignIn/Pages/SignInPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+using DMT.Models;
+using DMT.Services;
+using DMT.Controls;
+
+#endregion
+
+namespace DMT.Pages
+{
+    /// <summary>
+    /// Interaction logic for SignInPage.xaml
+    /// </summary>
+    public partial class SignInPage : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public SignInPage()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Loaded/Unloaded
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            SmartcardManager.Instance.UserChanged += Instance_UserChanged;
+            //txtUserId.SelectAll();
+            //txtUserId.Focus();
+        }
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+            SmartcardManager.Instance.UserChanged -= Instance_UserChanged;
+            SmartcardManager.Instance.Shutdown();
+        }
+
+        #endregion
+
+        #region Smartcard Handler(s)
+
+        private void Instance_UserChanged(object sender, EventArgs e)
+        {
+            /*
+            if (null != SmartcardManager.Instance.User)
+            {
+                _user = SmartcardManager.Instance.User;
+                if (tabs.SelectedIndex == 0)
+                {
+                    CheckUser();
+                }
+                else if (tabs.SelectedIndex == 1)
+                {
+                    txtUserId2.Text = _user.UserId;
+                    txtPassword2.SelectAll();
+                    txtPassword2.Focus();
+                }
+            }
+            */
+        }
+
+        #endregion
+    }
+}
---
+++ 06.Controls/DMT.Controls/SignIn/Windows/SignInWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+using DMT.Models;
+using DMT.Services;
+using DMT.Controls;
+
+#endregion
+
+namespace DMT.Windows
+{
+    /// <summary>
+    /// Interaction logic for SignInWindow.xaml
+    /// </summary>
+    public partial class SignInWindow : Window
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public SignInWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Loaded/Unloaded
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            SmartcardManager.Instance.UserChanged += Instance_UserChanged;
+            //txtUserId.SelectAll();
+            //txtUserId.Focus();
+        }
+
+        private void Window_Unloaded(object sender, RoutedEventArgs e)
+        {
+            SmartcardManager.Instance.UserChanged -= Instance_UserChanged;
+            SmartcardManager.Instance.Shutdown();
+        }
+
+        #endregion
+
+        #region Smartcard Handler(s)
+
+        private void Instance_UserChanged(object sender, EventArgs e)
+        {
+            /*
+            if (null != SmartcardManager.Instance.User)
+            {
+                _user = SmartcardManager.Instance.User;
+                if (tabs.SelectedIndex == 0)
+                {
+                    CheckUser();
+                }
+                else if (tabs.SelectedIndex == 1)
+                {
+                    txtUserId2.Text = _user.UserId;
+                    txtPassword2.SelectAll();
+                    txtPassword2.Focus();
+                }
+            }
+            */
+        }
+
+        #endregion
+    }
+}
