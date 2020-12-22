4a5a2768bb68409ef150cbbbfa3d3abf38765791
chumpon-asaneerat <chumponsenate@gmail.com>
2020-12-22 08:52:50 +0700

    Ported DMTv2 to DMT.TA.TODv1, DMT.TA.TODv2 #345 - DMT.TAxTODv2   - DMT.Controls project.     - Move all images from Backup to XXX folder -> git not stored backup folder.   - DMT.TA.App project.     - Refactor Button Style in SignInPage.   - DMT.TOD.App project.     - Refactor Button Style in SignInWindow     - Add JobListPage     - JobListPage Refactor Button Style

---
+++ 09.App/DMT.TOD.App/TOD/Pages/JobList/JobListPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+using DMT.Models;
+using DMT.Services;
+using NLib.Services;
+using NLib.Reflection;
+
+#endregion
+
+namespace DMT.TOD.Pages.Jobs
+{
+    /// <summary>
+    /// Interaction logic for JobListPage.xaml
+    /// </summary>
+    public partial class JobListPage : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public JobListPage()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Button Handlers
+
+        private void cmdOk_Click(object sender, RoutedEventArgs e)
+        {
+            // Main Menu Page
+            var page = new Menu.MainMenu();
+            PageContentManager.Instance.Current = page;
+        }
+
+        private void cmdCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Refresh();
+        }
+
+        #endregion
+
+        private void Refresh()
+        {
+
+        }
+    }
+}
